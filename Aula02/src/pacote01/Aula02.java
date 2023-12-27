427fcdf668fb325129fb62d2b8aa62e0ecd0550c
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2023-12-27 05:03:30 -0300

    criei um segundo Objeto Caneta

--- Aula02/src/pacote01/Aula02.java
+++ Aula02/src/pacote01/Aula02.java
@@ -7,6 +7,18 @@ public class Aula02 {
       c1.cor = "Azul";
       c1.ponta = 0.5f;
       c1.tampada = false;
+      c1.tampar();
       c1.status();
+      c1.rabiscar();
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "Compactor";
+        c2.cor = "Preto";
+        c2.ponta = 0.7f;
+        c2.carga = 90;
+        c2.tampada = true;
+
+        c2.status();
+        c2.rabiscar();
     }
 }
--- Aula02/src/pacote01/Caneta.java
+++ Aula02/src/pacote01/Caneta.java
@@ -8,16 +8,25 @@ public class Caneta {
     boolean tampada;
 
         void status(){
-            System.out.println("Uma caneta " + this.cor + " Com a Ponta "+ this.ponta);
+            System.out.println("Modelo: " + this.modelo);
+            System.out.println("Cor: "+ this.cor);
+            System.out.println("Ponta: "+ this.ponta);
+            System.out.println("Carga: "+ this.carga+"%.");
+            System.out.println("Está Tampada? "+ this.tampada);
         }
 
         void rabiscar(){
+                if (this.tampada == true){
+                    System.out.println("ERRO! Não Posso rabiscar");
+                } else{
+                    System.out.println("Pronta para Rabiscar");
+                }
 
         }
         void tampar(){
-
+                this.tampada = true;
         }
         void destampar(){
-            
+                this.tampada = false;
         }
 }
