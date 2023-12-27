9e11dcd8986beb2bb2d42a4df3a2450f47a7c0b3
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2023-12-27 04:36:55 -0300

    Primeiro Objeto criado
    Criei o Primeiro Objeto (Caneta), Aprendendo o que são classes e Pacotes

---
+++ Aula02/src/pacote01/Caneta.java
@@ -0,0 +1,23 @@
+package pacote01;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+        void status(){
+            System.out.println("Uma caneta " + this.cor + " Com a Ponta "+ this.ponta);
+        }
+
+        void rabiscar(){
+
+        }
+        void tampar(){
+
+        }
+        void destampar(){
+            
+        }
+}
