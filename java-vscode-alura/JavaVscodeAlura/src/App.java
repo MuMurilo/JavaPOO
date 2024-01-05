771d8d1c03588b90746ebf1c0759ce93508e0fcb
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2024-01-04 23:43:41 -0300

    testando funcionalidades do vs code

--- java-vscode-alura/JavaVscodeAlura/src/App.java
+++ java-vscode-alura/JavaVscodeAlura/src/App.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var artigo = new Artigo("Vs Code code", new Autor("Jacqueline oliveira"), LocalDate.of(2022, 07, 13) , Categoria.BACKEND);
+
+        System.out.println(artigo);
     }
 }
