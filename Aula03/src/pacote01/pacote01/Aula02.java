06005e24f1053469363b820832e7769f54cfbbe4
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2024-01-05 03:47:07 -0300

    erro no vs code

---
+++ Aula03/src/pacote01/pacote01/Aula02.java
@@ -0,0 +1,29 @@
+package pacote01;
+
+public class Aula02 {
+    public static void main(String[] args) {
+
+      Caneta c1  = new Caneta();
+
+      c1.status();
+
+
+        // Caneta c2 = new Caneta();
+        // c2.modelo = "Compactor";
+        // c2.cor = "Preto";
+        // c2.ponta = 0.7f;
+        // c2.carga = 90;
+        // c2.tampada = true;
+
+        // c2.status();
+        // c2.rabiscar();
+
+        // Sentimento s1 = new Sentimento();
+        // s1.tipo = "Amor";
+        // s1.intensidade = "Forte";
+        // s1.motivo = "Porque ela é 10/10";
+        // s1.ativo = false;
+        // s1.sentir();
+        // s1.status();
+    }
+}
