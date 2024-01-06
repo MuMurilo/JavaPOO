0beb6a57fbddbae4a447f57fd7ec17e2a767e5c6
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2024-01-06 04:25:49 -0300

    Aula 4

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,16 @@
+package aula04;
+
+public class Aula04 {
+
+    public static void main (String[] args){
+
+       Caneta c1 = new Caneta("Bic","Vermelha",0.5f);
+       //c1.setModelo("Compactor");
+       //c1.modelo ="Bic";
+       //c1.setPonta(0.7f);
+       //c1.ponta  = 0.5f;
+       c1.status();
+    }
+
+
+}
