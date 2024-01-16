08830ee5f2a203bdb974046f736cf5447f40ad31
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2024-01-16 09:31:25 -0300

    Relacionamento entre Classes
    aula 07

---
+++ Aula07/src/pacote07/Aula06.java
@@ -0,0 +1,7 @@
+package pacote07;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Aula07/src/pacote07/Lutador.java
@@ -0,0 +1,125 @@
+package pacote07;
+
+public class Lutador {
+
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+    public void apresentar(){
+
+    }
+    public void status(){
+
+    }
+    public void ganharLuta(){
+
+    }
+    public void perderLuta(){
+
+    }
+    public void empatarLuta(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String no) {
+        this.nome = no;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float pe) {
+        this.peso = pe;
+        setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String ca) {
+        this.categoria = ca;
+        if (peso<52.2){
+            this.categoria = "Inválido";
+        } else if (peso<=70.3) {
+            this.categoria= "Leve";
+        } else if (peso<=83.9) {
+            this.categoria = "Médio";
+        } else if (peso<=120.2) {
+            this.categoria = "Pesado";
+        } else if (peso>120.2) {
+            this.categoria = "Inválido";
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public Lutador(String no, String na, int id, float al, float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.setPeso(pe);
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+}
