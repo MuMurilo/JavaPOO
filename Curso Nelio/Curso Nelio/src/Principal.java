c0669b5f18e84469dd463274c704d2315d2961c4
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2025-11-11 11:57:18 -0300

    Nelio aula 6

---
+++ Curso Nelio/Curso Nelio/src/Principal.java
@@ -0,0 +1,20 @@
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        String product1 = "Computer";
+        String product2 = "Office desk";
+    
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+    
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+    
+        System.out.printf("Products:%n %s, which prise is $ %f%n ");
+    
+    }
+
+
+
+}
