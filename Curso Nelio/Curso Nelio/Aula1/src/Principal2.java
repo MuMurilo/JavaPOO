34e147327738c4ebbea1afa9d2f4ebfde6aec330
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2025-11-11 22:27:01 -0300

    Exercicios

---
+++ Curso Nelio/Curso Nelio/Aula1/src/Principal2.java
@@ -0,0 +1,18 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Principal2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        double pi = 3.14159;
+        double raio;
+        double area;
+        raio = sc.nextDouble();
+
+        area = pi * Math.pow(raio,2.0);
+        System.out.printf("A Area do Circulo é: %.4f", area);
+    }
+}
